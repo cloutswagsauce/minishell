d15fc455b7061b122ce7e6c71933db557ad54771
Lucca Faria Mourao <lfaria-m@c3r2s3.42lausanne.ch>
2024-12-02 19:24:03 +0100

    first working version

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lfaria-m <lfaria-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 10:24:57 by lfaria-m          #+#    #+#             */
-/*   Updated: 2024/12/02 16:50:42 by lfaria-m         ###   ########.fr       */
+/*   Updated: 2024/12/02 19:12:10 by lfaria-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,20 @@ int main(void)
 {
 	char *input;
 	pid_t pid;
-	t_com command;
+	//t_com command;
 
-	printf("%s", getenv("PATH"));
 	while (1)
 	{
 
 		input = readline("mini$hell: ");
 		if (*input)
 		{
-			command = parse_input(input);
-			add_history(input);
 			pid = fork();
 			if (!pid)
 			{
-				execve("/bin/ls", command.argv, 0);
+				parse_input(input);
+				add_history(input);
+
 				exit(0);
 			}
 			wait(0);
--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: lfaria-m <lfaria-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 12:59:34 by lfaria-m          #+#    #+#             */
-/*   Updated: 2024/12/02 17:27:54 by lfaria-m         ###   ########.fr       */
+/*   Updated: 2024/12/02 19:22:45 by lfaria-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,28 +21,34 @@ void	free_command(t_com command)
 void path_split_append(t_com command)
 {
     char	**path_split;
-	struct	stat *buff;
 	char	*exec_path;
+	int		len;
 
-	buff = 0;
 
     path_split = ft_split(getenv("PATH"), ':');
 
-	exec_path = *path_split;
-	while (exec_path)
+	while (*path_split)
 	{
-		ft_strlcat(exec_path, "/", ft_strlen(exec_path) + 1);
-		ft_strlcat(exec_path, command.argv[0], join_len(exec_path, command.argv[0]));
-		if (!stat(exec_path, buff))
+		len = join_len(*path_split, command.argv[0]) + 2;
+		exec_path = malloc(len); // 1  for null and 1 for /;
+		ft_memcpy(exec_path, *path_split, ft_strlen(*path_split));
+		ft_strlcat(exec_path, "/", len);
+		ft_strlcat(exec_path, command.argv[0], len);
+		exec_path[len-1] = '\0';
+		if (!access(exec_path, F_OK))
 		{
-			printf("found that shit");
+			printf("found it: %s\n", exec_path);
+			execv(exec_path, command.argv);
+			free(exec_path);
+			break;
 		}
-		exec_path++;
+			
+		else
+		{
+			path_split++;
+		}
+			
 	}
-	
-
-
-
 }
 t_com	parse_input(char *str)
 {
