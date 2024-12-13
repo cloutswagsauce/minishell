41b7c2e0c8f1484bbd5ad51e402281aadd85fc60
luccamourao <lfaria@epfl.ch>
2024-12-13 13:11:41 +0100

    environment variables working with built in commands, before executing, it checks if there is a dollar sign, if there is, it gets replaced by value

--- srcs/handle_variable.c
+++ srcs/handle_variable.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   handle_variable.c                                  :+:      :+:    :+:   */
@@ -6,38 +6,60 @@
 /*   By: lfaria-m <lfaria-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 10:59:13 by lfaria-m          #+#    #+#             */
-/*   Updated: 2024/12/13 11:42:09 by lfaria-m         ###   ########.fr       */
+/*   Updated: 2024/12/13 13:09:51 by lfaria-m         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "../minishell.h"
 
-void expand_variable(t_com *com, int i)
+void	expand_variable(t_com *com, int i)
 {
-    char *new_value;
+	char	*new_value;
 
-    
-    if (getenv(com->argv[i]+1))
-    {
-        new_value = ft_strdup(getenv(com->argv[i]+1));
-        if (!new_value)
-            return ;
-        free(com->argv[i]);
-        com->argv[i] = new_value;
-    }
-    return ;
+	if (getenv(com->argv[i] + 1))
+	{
+		new_value = ft_strdup(getenv(com->argv[i] + 1));
+		if (!new_value)
+			return ;
+		free(com->argv[i]);
+		com->argv[i] = new_value;
+	}
+	return ;
 }
 
-void command_has_variable(t_com *com, t_list *local_env)
+void	expand_local_variable(t_com *com, int i, t_list *vars)
 {
-    int i;
+	char	*new_value;
 
-    i = 0;
-    while (com->argv[i])
-    {
-        if (strchr(com->argv[i], '$'))
-            expand_variable(com, i);
-        i++;
-    }
-    
+	while (vars)
+	{
+		if (!memcmp(com->argv[i] + 1, vars->name, ft_strlen(com->argv[i])))
+		{
+			new_value = ft_strdup(vars->value);
+			free(com->argv[i]);
+			com->argv[i] = new_value;
+			break ;
+		}
+		vars = vars->next;
+	}
+}
+
+void	command_has_variable(t_com *com, t_list *local_env)
+{
+	int	i;
+
+	i = 0;
+	while (com->argv[i])
+	{
+		if (strchr(com->argv[i], '$'))
+			expand_variable(com, i);
+		i++;
+	}
+	i = 0;
+	while (com->argv[i])
+	{
+		if (strchr(com->argv[i], '$'))
+			expand_local_variable(com, i, local_env);
+		i++;
+	}
 }
