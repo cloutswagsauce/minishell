d0e4ae5120e92fb493453b72eef66989dd455533
luccamourao <lfaria@epfl.ch>
2024-12-14 15:16:44 +0100

    done

--- srcs/handle_variable.c
+++ srcs/handle_variable.c
@@ -6,43 +6,40 @@
 /*   By: lfaria-m <lfaria-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 10:59:13 by lfaria-m          #+#    #+#             */
-/*   Updated: 2024/12/13 13:34:25 by lfaria-m         ###   ########.fr       */
+/*   Updated: 2024/12/14 15:16:16 by lfaria-m         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "../minishell.h"
 
-void	expand_variable(t_com *com, int i)
+void expand_variable(t_com *com, int i, t_list *vars)
 {
-	char	*new_value;
+    char    *new_value;
 
-	if (getenv(com->argv[i] + 1))
-	{
-		new_value = ft_strdup(getenv(com->argv[i] + 1));
-		if (!new_value)
-			return ;
-		free(com->argv[i]);
-		com->argv[i] = new_value;
-	}
-	return ;
+    if (getenv(com->argv[i] + 1))
+    {
+        new_value = ft_strdup(getenv(com->argv[i] + 1));
+        if (!new_value)
+            return ;
+        free(com->argv[i]);
+        com->argv[i] = new_value;
+        return ;
+    }
+    while (vars)
+    {
+        if (!ft_memcmp(com->argv[i] + 1, vars->name, ft_strlen(com->argv[i])))
+        {
+            new_value = ft_strdup(vars->value);
+            free(com->argv[i]);
+            com->argv[i] = new_value;
+            return ;
+        }
+        vars = vars->next;
+    }
+    free(com->argv[i]);
+	com->argv[i] = ft_strdup("");
 }
 
-void	expand_local_variable(t_com *com, int i, t_list *vars)
-{
-	char	*new_value;
-
-	while (vars)
-	{
-		if (!ft_memcmp(com->argv[i] + 1, vars->name, ft_strlen(com->argv[i])))
-		{
-			new_value = ft_strdup(vars->value);
-			free(com->argv[i]);
-			com->argv[i] = new_value;
-			break ;
-		}
-		vars = vars->next;
-	}
-}
 
 void	command_has_variable(t_com *com, t_list *local_env)
 {
@@ -52,14 +49,8 @@ void	command_has_variable(t_com *com, t_list *local_env)
 	while (com->argv[i])
 	{
 		if (ft_strchr(com->argv[i], '$'))
-			expand_variable(com, i);
+			expand_variable(com, i, local_env);
 		i++;
 	}
-	i = 0;
-	while (com->argv[i])
-	{
-		if (ft_strchr(com->argv[i], '$'))
-			expand_local_variable(com, i, local_env);
-		i++;
-	}
+	
 }
